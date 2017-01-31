f08196de02ef3247b7594845e5de7ffad4308b7b
ITLAB <ITLAB@IT>
2017-01-31 14:29:33 +0530

    first calculator

---
+++ src/com/example/native_calc/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.native_calc;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+
+
+public class MainActivity extends Activity {
+
+    Button button0 , button1 , button2 , button3 , button4 , button5 , button6 ,
+            button7 , button8 , button9 , buttonAdd , buttonSub , buttonDivision ,
+            buttonMul , button10 , buttonC , buttonEqual ;
+
+    EditText edt1 ;
+
+    float mValueOne , mValueTwo ;
+
+    boolean mAddition , mSubtract ,mMultiplication ,mDivision ;
+
+    @Override
+}
